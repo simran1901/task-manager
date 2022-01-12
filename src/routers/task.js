da4de23d53c5e2c8abda71cd967fcc02e29c61eb
Simran Makhijani <simranmakhijani55@gmail.com>
2022-01-13 01:05:37 +0530

    pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -16,11 +16,26 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
     try {
-        const tasks = await Task.find({ owner: req.user._id })
-        // await req.user.populate('tasks').execPopulate()
-        res.send(tasks)
+        // const tasks = await Task.find({ owner: req.user._id })
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
+        })
+        res.send(req.user.tasks)
     } catch (e) {
         console.log(e)
         res.status(500).send()
