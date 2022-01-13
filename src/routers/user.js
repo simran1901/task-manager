54299d3b0db849c0ab4a83addb4b2396d665a225
Simran Makhijani <simranmakhijani55@gmail.com>
2022-01-13 19:47:31 +0530

    Working on image upload

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const multer = require('multer')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -80,5 +82,45 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error('')
+        }
+
+        res.set('Content-Type', 'image/jpeg')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
+})
 
 module.exports = router
